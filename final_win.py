be7fe28f29b5ba124f37ef8f2e10aa8be6b3c1ed
Максим <106909321+kennrox@users.noreply.github.com>
2022-06-05 17:58:53 +0700

    Update final_win.py
    Добавлена библиотека

--- final_win.py
+++ final_win.py
@@ -1,1 +1,4 @@
-
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont #
+from PyQt5.QtWidgets import (QApplication, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout, QGroupBox, QRadioButton, QPushButton, QLabel, QListWidget, QLineEdit)
+from instr import *
